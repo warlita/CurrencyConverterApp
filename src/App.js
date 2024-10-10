ba6de043b1b964c424a8b3c7808615160586d54a
Analita <88949609+warlita@users.noreply.github.com>
2024-10-09 19:52:49 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <a href="https://www.flaticon.com/free-stickers/under-construction" title="under construction stickers">Under construction stickers created by Design Circle - Flaticon</a>
+        <p>
+         Currency Converter Comiing Soon!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
